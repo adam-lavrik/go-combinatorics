f06cb27582480fde58be554367326079fc360d88
adam-lavrik <lavrik.adam@gmail.com>
2022-05-28 13:43:17 +0300

    Add combinatorial functions

---
+++ combinatorics.go
@@ -0,0 +1,124 @@
+/*
+Copyright 2022 Adam Lavrik <lavrik.adam@gmail.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+*/
+package combinatorics
+
+type Count = uint64
+
+// Factorial returns factorial
+// n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 1
+// 0! = 1
+func Factorial(n Count) Count {
+	return product(2, n)
+}
+
+// Multifactorial returns multifactorial
+// n!...(m) = n * (n - m) * (n - 2m) * ... (while (n - km) > 0)
+// 0!...(m) = 1
+// n!...(m) = 0 if m = 0 or n < m
+func Multifactorial(n, m Count) Count {
+	if m == 0 || n < m {
+		return 0
+	}
+	var p Count = 1
+	for m <= n {
+		p *= n
+		n -= m
+	}
+	return p
+}
+
+// PCount returns number of permutations without repetitions
+// P(n, m) = n! / (n - m)! = n * (n - 1) * ... * (n - m + 1)
+// P(n, m) = 0 if n < m
+func PCount(n, m Count) Count {
+	if n < m {
+		return 0
+	}
+	return product(n - m + 1, n)
+}
+
+// PrCount returns number of permutations with repetitions
+// Pr(m0, m1, ...) = (m0 + m1 + ...)! / m0! / m1! / ...
+// Pr() = Pr(0, 0, ...) = 1
+func PrCount(m ...Count) Count {
+	i := len(m) - 1
+	if i < 1 {
+		return 1
+	}
+	var p, s Count = 1, m[i] + 1
+	var sx Count
+	for i > 0 {
+		i--
+		sx = s + m[i]
+		p *= dividedProduct(s, sx - 1)
+		s = sx
+	}
+	return p
+}
+
+// CCount returns number of combinations without repetitions
+// C(n, m) = n! / (n - m)! / m! = n / 1 * (n - 1) / 2 * ... * (n - m + 1) / n
+// C(n, m) = 0 if n < m
+func CCount(n, m Count) Count {
+	if n < m {
+		return 0
+	}
+	return dividedProduct(n - m + 1, n)
+}
+
+// CrCount returns number of combinations with repetitions
+// Cr(n, m) = C(n + m - 1, m)
+// Cr(0, 0) = 0
+func CrCount(n, m Count) Count {
+	if n|m == 0 {
+		return 0
+	}
+	return dividedProduct(n, n + m - 1)
+}
+
+// WCount returns number of m-letter words of n-letter alphabet 
+// W(n, m) = n ** m
+// W(0, 0) = 1
+func WCount(n, m Count) Count {
+	var p Count = 1
+	for m != 0 {
+		if m & 1 == 1 {
+			p *= n
+		}
+		n *= n
+		m >>= 1
+	}
+	return p
+}
+
+// product returns l * (l + 1) * ... * u
+func product(l, u Count) Count {
+	var p Count = 1
+	for l <= u {
+		p *= l
+		l++
+	}
+	return p
+}
+
+// dividedProduct returns l / 1 * (l + 1) / 2 * ... * u / (u - l + 1)
+func dividedProduct(l, u Count) Count {
+	var p Count = 1
+	var d Count = 0
+	for l <= u {
+		d++
+		p = p * l / d
+		l++
+	}
+	return p
+}
